cd62380fc0c13d061754d1cafaf0400906665d42
Hudson Holanda <hudsonholanda55@gmail.com>
2021-10-14 19:34:10 -0300

    feat: create html from hero section

--- front-page.php
+++ front-page.php
@@ -1,4 +1,47 @@
+<?php
+include 'inc/variables.php';
+?>
 <?php get_header(); ?>
 
+<main>
+  <section class="hero">
+    <div class="hero__container">
+      <div class="hero__content">
+        <h1 class="hero__title">
+          SPACE<span class="highlight">.</span>
+        </h1>
+        <p class="hero__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie elit at lacus…
+        </p>
+        <a href="" class="hero__btn btn btn--small btn--blue">
+          click
+        </a>
+      </div>
+      <div class="hero__trending-today">
+        <div class="hero__trending-today-column">
+          <p class="hero__trending-today-text hero__trending-today-text--title">
+            Trending
+            <span class="highlight">Today</span>
+          </p>
+        </div>
+        <div class="hero__trending-today-column">
+          <p class="hero__trending-today-text">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing ligula eget dolor.
+          </p>
+        </div>
+        <div class="hero__trending-today-column">
+          <p class="hero__trending-today-text">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing ligula eget dolor.
+          </p>
+        </div>
+        <div class="hero__trending-today-column">
+          <p class="hero__trending-today-text">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing ligula eget dolor.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
 
 <?php get_footer(); ?>
