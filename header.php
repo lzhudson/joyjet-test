e9eb40380c94e84e39a3fbbbb7509f0392e559e9
Hudson Holanda <hudsonholanda55@gmail.com>
2021-10-14 21:26:53 -0300

    feat: import style from glider.js

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Joy Jet Test</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1/glider.min.css">
   <?php wp_head(); ?>
 </head>
 <body>
