72b0b4dd64c0c3d26f2d9dda0e5e452e4aee744c
Hudson Holanda <hudsonholanda55@gmail.com>
2021-10-16 11:03:50 -0300

    feat: integrate section about

--- front-page.php
+++ front-page.php
@@ -70,6 +70,7 @@ include 'inc/variables.php';
             </div>
           </div>
           <?php endforeach;?>
+          <?php wp_reset_postdata();?>
         </div>
         <button aria-label="Previous" class="glider-prev">
           <img src="<?php echo $pathToImages . 'arrow-left.svg'; ?>" alt="">
@@ -82,22 +83,20 @@ include 'inc/variables.php';
 
     </div>
   </section>
+  <?php
+    $imageAbout = get_field('image');
+    $imageAboutUrl = $imageAbout['url'];
+    $imaegAboutAlt = $imageAbout['alt'];
+    $titleSectionAbout = get_field('title');
+    $contentSectionAbout = get_field('content');
+  ?>
   <section id="about" class="about-us">
     <div class="about-us__container">
-      <img src="<?php echo $pathToImages . 'image-about-us.jpg'; ?>" alt="About JoyJet">
+      <img src="<?php echo $imageAboutUrl; ?>" alt="<?php echo $imaegAboutAlt;?>">
       <div class="about-us__content">
-        <h2 class="about-us__title">About US</h2>
+        <h2 class="about-us__title"><?php echo $titleSectionAbout;?></h2>
         <p class="about-us__text">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-          Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-          <br><br>
-          In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
-          mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
-          vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-          enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-          Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+          <?php echo $contentSectionAbout; ?>
         </p>
       </div>
     </div>
