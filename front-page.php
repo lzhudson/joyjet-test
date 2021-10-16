aff32ce23c69782216896b471a7a81f3da7b2c27
Hudson Holanda <hudsonholanda55@gmail.com>
2021-10-16 08:46:22 -0300

    feat: add structure from section about

--- front-page.php
+++ front-page.php
@@ -139,6 +139,26 @@ include 'inc/variables.php';
 
     </div>
   </section>
+  <section class="about-us">
+    <div class="about-us__container">
+      <img src="<?php echo $pathToImages . 'image-about-us.jpg'; ?>" alt="About JoyJet">
+      <div class="about-us__content">
+        <h2 class="about-us__title">About US</h2>
+        <p class="about-us__text">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+          Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+          <br><br>
+          In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+          mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
+          vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+          enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+          Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+        </p>
+      </div>
+    </div>
+  </section>
 </main>
 
 <?php get_footer(); ?>
