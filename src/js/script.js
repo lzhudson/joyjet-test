d7f2af3301ce4528489f2e111438ceeb73b88c38
Hudson Holanda <hudsonholanda55@gmail.com>
2021-10-16 09:54:38 -0300

    feat: import and instance smooth scroll script

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 import BlogSlider from "./components/blog-slider";
 import MenuMobile from "./components/menu-mobile";
+import SmoothScroll from "./components/smooth-scroll";
 import StickyNav from "./components/sticky-nav";
 
 const menuMobile = new MenuMobile();
 const blogSlider = new BlogSlider();
 const stickyNav = new StickyNav();
+const smoothScroll = new SmoothScroll();
