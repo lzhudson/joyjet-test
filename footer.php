6ac00ee044d791ae0a03c5a69d532ecfc84bed62
Hudson Holanda <hudsonholanda55@gmail.com>
2021-10-16 08:59:42 -0300

    feat: create structure footer

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
-  <footer>
+  <footer class="footer">
+    <div class="footer__container">
+      <p class="footer__copyright">
+        © 2016 Created by Joyjet <br class="mobile-only"/> Digital Space Agency
+      </p>
+    </div>
   </footer>
   <?php wp_footer(); ?>
   </body>
